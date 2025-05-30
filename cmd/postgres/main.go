44abf60d41237b1f9c3af269b15d7d28da085457
Michael <michael.mikhalevich@gmail.com>
2025-05-30 20:53:20 +0300

    slice query provider (#5)

--- cmd/postgres/main.go
+++ cmd/postgres/main.go
@@ -13,46 +13,31 @@ const (
 
 func main() {
 	var (
-		pagin = paginator.New(NewData(), pageSize)
-		page  = &paginator.Page[int]{
-			PageTotalCount: pageSize,
-		}
+		pagin = paginator.New(NewSliceProvider(), pageSize)
+		page  *paginator.Page[int]
 	)
+
+	page, _ = pagin.Page(1)
+	printPage(page)
 
 	for page.HasNext() {
 		page, _ = pagin.Page(page.Next())
 
-		log.Printf("bottom index: %d top index: %d, page number: %d total pages: %d page data: %v",
-			page.BottomIndex, page.TopIndex, page.PageNumber, page.PageTotalCount, page.Data)
+		printPage(page)
 	}
 }
 
-type TestData struct {
-	Data []int
+func printPage(page *paginator.Page[int]) {
+	log.Printf("bottom index: %d top index: %d, page number: %d total pages: %d page data: %v",
+		page.BottomIndex, page.TopIndex, page.PageNumber, page.PageTotalCount, page.Data)
 }
 
-func NewData() *TestData {
+func NewSliceProvider() *paginator.SliceQueryProvider[int] {
 	data := make([]int, 0, dataLen)
 
 	for i := range dataLen {
 		data = append(data, i+1)
 	}
 
-	return &TestData{
-		Data: data,
-	}
-}
-
-func (t *TestData) Query(offset int, limit int) ([]int, error) {
-	limit = offset + limit
-
-	if limit > len(t.Data) {
-		limit = len(t.Data)
-	}
-
-	return t.Data[offset:limit], nil
-}
-
-func (t *TestData) Count() (int, error) {
-	return len(t.Data), nil
+	return paginator.NewSliceQueryProvider(data)
 }
